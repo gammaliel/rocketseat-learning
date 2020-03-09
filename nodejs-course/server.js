c83bdb7cf04c9966e9d8d4b71658cdd3ffac21e5
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2020-03-08 23:22:00 -0300

    Add node-js course

---
+++ nodejs-course/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+// Iniciando o App
+const app = express();
+
+// Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+requireDir("./src/models/");
+const Product = mongoose.model("Product");
+
+app.get("/", (req, res) => {
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with React",
+    url: "https://github.com/facebook/react-native"
+  });
+  return res.send("Hello dsada");
+});
+
+app.listen(3001);
